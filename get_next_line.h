e5eb82acb3951c021d86d5535db3e766af270807
benes-al <gryk_@hotmail.com>
2025-05-10 08:39:32 +0100

    update_functions

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: benes-al <benes-al@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 21:27:37 by benes-al          #+#    #+#             */
-/*   Updated: 2025/05/08 21:31:03 by benes-al         ###   ########.fr       */
+/*   Updated: 2025/05/10 08:35:22 by benes-al         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,9 @@
 //prototype functions
 
 char	*get_next_line(int fd);
+int		ft_strlen(char *line)
+char	*ft_strcat(char *buffer, char *line, int bytes_read)
+int		ft_verification(char *buffer)
+int		ft_strlen(char *line)
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,27 +6,52 @@
 /*   By: benes-al <benes-al@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 21:27:58 by benes-al          #+#    #+#             */
-/*   Updated: 2025/05/08 21:31:02 by benes-al         ###   ########.fr       */
+/*   Updated: 2025/05/10 08:36:11 by benes-al         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	verification(char *buffer)
+char	*ft_strcat(char *buffer, char *line, int bytes_read)
+{
+	char	*temp;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	temp = malloc(sizeof(char) * (bytes_read + ft_strlen(line) + 1));
+	if(!temp)
+		return (0);
+	while (line[i])
+	{
+		temp[i] = line[i];
+		i++;
+	}
+	while (buffer[j])
+	{
+		temp[i + j] = buffer[j];
+		j++;
+	}
+	temp[i + j] = '\0';
+	return (temp);
+}
+
+int	ft_verification(char *buffer)
 {
 	int	i;
 
 	i = 0;
-	while (buffer[i])
+	while (i < BUFFER_SIZE)
 	{
-		if (buffer[i] == '\n')
+		if (buffer[i] == '\n' || buffer [i] == '\0')
 			return (1);
 		i++;
 	}
 	return (0);
 }
 
-int	strlen(char *line)
+int	ft_strlen(char *line)
 {
 	int	i;
 
